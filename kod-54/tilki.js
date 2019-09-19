cba98d1c25c0ace19ee2da40fe3ad42693b74451
Bratva STARK <48415026+AirRollMurat@users.noreply.github.com>
2019-09-19 18:07:38 +0300

    Create tilki.js

---
+++ kod-54/tilki.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+
+exports.run = async(client, message, args) => {
+    let msg = await message.channel.send("Resim aranıyor...");
+
+    let {body} = await superagent 
+    .get('https://randomfox.ca/floof/');
+    if(!{body}) return message.channel.send("<a:h_:591907035553988638> Bir hata oluştu. Tekrar deneyiniz.")
+
+    const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setDescription("**"+message.author.username+"**, İşte bir tilki !")
+    .setImage(body.image)
+    .setTimestamp()
+    message.channel.send({embed})
+
+    msg.delete();
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['tilki'],
+  permLevel: 0
+};
+
+exports.help = {
+    name: "fox",
+    description: "Bir kedi resmi gönderir.",
+    usage: "(STARK)fox"
+};
