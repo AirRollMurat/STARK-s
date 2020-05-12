e204988ffff88392e5c79e1adfe144198665ad84
Bratva STARK <48415026+AirRollMurat@users.noreply.github.com>
2020-05-12 17:31:42 +0300

    Update top10.js

--- kod-67/top10.js
+++ kod-67/top10.js
@@ -5,9 +5,9 @@ module.exports = {
     const { MessageEmbed } = require("discord.js");
     let sunucular = stark.guilds.cache.array().sort((a, b) => b.memberCount - a.memberCount).slice(0, 10);
     const top = stark.guilds.cache.sort((a,b)=>a.memberCount-b.memberCount).array().reverse()
-    let stark = [
+    let murat = [
       ["Sunucu İsmi", "Üye Sayısı"],
     ]
-    sunucular.forEach(a => stark.push([a.name, a.memberCount]))
+    sunucular.forEach(a => murat.push([a.name, a.memberCount]))
     await msg.channel.send(new MessageEmbed().setColor("BLACK").setDescription(`\`\`\`fix\n${table(stark)}\`\`\``))
 };
